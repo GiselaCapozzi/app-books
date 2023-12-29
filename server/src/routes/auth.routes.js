25125be79862619c9b318351cdf766cc224b82b1
GiselaCapozzi <gisecapozzi@gmail.com>
2023-12-29 17:48:01 -0300

    Obtener los datos de un usuario por su id

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,7 +1,12 @@
 import { Router } from 'express';
-import { login, register } from '../controllers/auth.controllers.js';
+import { 
+  getUserById, 
+  login, 
+  register 
+} from '../controllers/auth.controllers.js';
 
 export const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/:id', getUserById);
