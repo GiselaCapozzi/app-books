e17162d8d705bfccc49411bd389c0e39517c1585
GiselaCapozzi <gisecapozzi@gmail.com>
2024-01-05 12:09:09 -0300

    Página de incio con link para ir al perfil del usuario

--- client/src/components/Icon.jsx
+++ client/src/components/Icon.jsx
@@ -1,14 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-const Icon = ({ icon, color, size, onClick }) => {
+const Icon = ({ icon, color, size, onClick, className }) => {
 
   return (
-    <FontAwesomeIcon 
+    <FontAwesomeIcon
       icon={icon}
       color={color}
       size={size}
-      className="cursor-pointer"
-      onClick={onClick}   
+      className={`cursor-pointer ${className}`}
+      onClick={onClick}
     />
   )
 }
