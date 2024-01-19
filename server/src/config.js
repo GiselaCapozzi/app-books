64c6191cea35a42300b9f237016a2ed5c5f2f6ad
GiselaCapozzi <gisecapozzi@gmail.com>
2024-01-19 12:18:53 -0300

    Command start add

---
+++ server/src/config.js
@@ -0,0 +1,6 @@
+export const PORT = process.env.PORT || 4000;
+export const HOST = process.env.HOST || 'localhost';
+export const USER = process.env.USER || 'root';
+export const PASSWORD = process.env.PASSWORD || 'Far8519ganjulio06%';
+export const DATABASE = process.env.DATABASE || 'booksdb';
+export const DB_PORT = process.env.DB_PORT || 3306;
